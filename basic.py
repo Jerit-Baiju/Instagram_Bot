6eaeb1e33d5363f6d4f6251d8f4f05f3068fa9f0
Jerit-Baiju <jeritalumkal@gmail.com>
2022-05-08 11:15:35 +0530

    alter

--- basic.py
+++ basic.py
@@ -1,9 +1,5 @@
 import random
 
-def inp():
-    return input(f"--> ").replace("?", "").replace("_",
-                                                   "").casefold().lstrip().rstrip()
-
 
 def sub(text):
     return input(f"--> {str(text).upper()} > ").replace("?", "").casefold().lstrip().rstrip()
@@ -14,20 +10,3 @@ def out(value):
         return print(f">>> {str(value).upper()} .")
     elif type(value) == list:
         return print(f">>> {str(random.choice(value)).upper()} .")
-
-
-def greet(name):
-    out(["hi there!", "hai there!", f"welcome {name}", "Greetings from abettor", f"hai {name}"])
-
-
-def check_all(list, split):
-    return all(value in split for value in list)
-
-
-def compare(list, main_word, split):
-    if main_word in split:
-        split.pop(split.count(main_word))
-        for other_word in list:
-            if other_word in split:
-                return True
-
